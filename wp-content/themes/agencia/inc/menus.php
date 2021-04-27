3b35d3554b7fef63ead663a6e5521ef560b9f7ef
melch <clement.passet@gmail.com>
2021-04-27 07:25:30 +0200

    Ajout des  widgets dans le footer

--- wp-content/themes/agencia/inc/menus.php
+++ wp-content/themes/agencia/inc/menus.php
@@ -2,3 +2,14 @@
 add_action('after_setup_theme',function(){
     register_nav_menu('header', __('Main navigation','agencia'));
 });
+
+add_action('widgets_init', function(){
+    register_sidebar([
+        'id'=>'footer',
+        'name'=>__('Footer', 'agencia'),
+        'before_title'=>'<div class="footer__title">',
+        'after_title'=> '</div>',
+        'before_widget'=>'<div class="footer__col">',
+        'after_widget'=>'</div>'
+    ]);
+});
