03792c442815b3bc6891c3b572b85fc3d110333a
melch <clement.passet@gmail.com>
2021-04-30 20:46:39 +0200

    Page single d'un bien

--- wp-content/plugins/agence/agence.php
+++ wp-content/plugins/agence/agence.php
@@ -1,46 +1,47 @@
 <?php
+
 /**
  * Plugin Name: Agence Plugin
  * Description: Plugin to manage the properties of the agency.
  */
-add_action('init', function(){
-    register_post_type('property',[
+add_action('init', function () {
+    register_post_type('property', [
         'label' => __('Properties', 'agence'),
-        'labels'=>[
-            'name'=> __('Properties','agence'),
-            'singular_name'=> __('Property','agence'),
-            'edit_item'=> __('Edit property','agence'),
-            'new_item'=> __('New property','agence'),
-            'view_item'=> __('View property','agence'),
-            'view_items'=> __('View properties','agence'),
-            'search_items'=> __('Search properties','agence'),
-            'not_found'=> __('No properties found.','agence'),
-            'not_found_in_trash'=> __('No properties found in trash.','agence'),
-            'all_items'=> __('All properties','agence'),
-            'archives'=> __('Property archive','agence'),
-            'attributes'=> __('Property attributes','agence'),
-            'insert_into_item'=> __('Insert into property','agence'),
-            'uploaded_to_this_item'=> __('Uploaded to this property','agence'),
-            'filter_items_list'=> __('Filter properties list','agence'),
-            'items_list_navigation'=> __('Properties list navigation','agence'),
-            'items_list'=> __('Properties list','agence'),
-            'item_published'=> __('Property published','agence'),
-            'item_published_privately'=> __('Property published privately','agence'),
-            'item_reverted_to_draft'=> __('Property reverted to draft','agence'),
-            'item_scheduled'=> __('Property scheduled','agence'),
-            'item_updated'=> __('Property updated','agence')
+        'labels' => [
+            'name' => __('Properties', 'agence'),
+            'singular_name' => __('Property', 'agence'),
+            'edit_item' => __('Edit property', 'agence'),
+            'new_item' => __('New property', 'agence'),
+            'view_item' => __('View property', 'agence'),
+            'view_items' => __('View properties', 'agence'),
+            'search_items' => __('Search properties', 'agence'),
+            'not_found' => __('No properties found.', 'agence'),
+            'not_found_in_trash' => __('No properties found in trash.', 'agence'),
+            'all_items' => __('All properties', 'agence'),
+            'archives' => __('Property archive', 'agence'),
+            'attributes' => __('Property attributes', 'agence'),
+            'insert_into_item' => __('Insert into property', 'agence'),
+            'uploaded_to_this_item' => __('Uploaded to this property', 'agence'),
+            'filter_items_list' => __('Filter properties list', 'agence'),
+            'items_list_navigation' => __('Properties list navigation', 'agence'),
+            'items_list' => __('Properties list', 'agence'),
+            'item_published' => __('Property published', 'agence'),
+            'item_published_privately' => __('Property published privately', 'agence'),
+            'item_reverted_to_draft' => __('Property reverted to draft', 'agence'),
+            'item_scheduled' => __('Property scheduled', 'agence'),
+            'item_updated' => __('Property updated', 'agence')
         ],
-        'public'=>true,
-        'hierarchical'=>false,
-        'exclude_from_search'=>false,
-        'supports'=>['title','editor','excerpt','thumbnail'],
-        'menu_icon'=>'dashicons-admin-multisite',
-        'menu_position'=>3,
-        'taxonomies'=>['property_type','property_city','property_option']
+        'public' => true,
+        'hierarchical' => false,
+        'exclude_from_search' => false,
+        'supports' => ['title', 'editor', 'excerpt', 'thumbnail'],
+        'menu_icon' => 'dashicons-admin-multisite',
+        'menu_position' => 3,
+        'taxonomies' => ['property_type', 'property_city', 'property_option']
     ]);
-    register_taxonomy('property_type','property',[
-        'meta_box_cb'=>'post_categories_meta_box',
-        'labels'=>[
+    register_taxonomy('property_type', 'property', [
+        'meta_box_cb' => 'post_categories_meta_box',
+        'labels' => [
             'name' => __('Types', 'agence'),
             'singular_name' => __('Type', 'agence'),
             'search_items' => __('Search types', 'agence'),
@@ -61,9 +62,9 @@ add_action('init', function(){
             'back_to_items' => __('&larr; Back to types', 'agence')
         ]
     ]);
-    register_taxonomy('property_city','property',[
-        'meta_box_cb'=>'post_categories_meta_box',
-        'labels'=>[
+    register_taxonomy('property_city', 'property', [
+        'meta_box_cb' => 'post_categories_meta_box',
+        'labels' => [
             'name' => __('Cities', 'agence'),
             'singular_name' => __('City', 'agence'),
             'search_items' => __('Search Cities', 'agence'),
@@ -85,9 +86,9 @@ add_action('init', function(){
             'back_to_items' => __('&larr; Back to Cities', 'agence')
         ]
     ]);
-    register_taxonomy('property_option','property',[
-        'meta_box_cb'=>'post_categories_meta_box',
-        'labels'=>[
+    register_taxonomy('property_option', 'property', [
+        'meta_box_cb' => 'post_categories_meta_box',
+        'labels' => [
             'name' => __('Options', 'agence'),
             'singular_name' => __('Option', 'agence'),
             'search_items' => __('Search Options', 'agence'),
@@ -111,5 +112,29 @@ add_action('init', function(){
     ]);
 });
 
-register_activation_hook(__FILE__,'flush_rewrite_rules');
-register_deactivation_hook(__FILE__,'flush_rewrite_rules');
+register_activation_hook(__FILE__, 'flush_rewrite_rules');
+register_deactivation_hook(__FILE__, 'flush_rewrite_rules');
+
+/**
+ * Method agence_city
+ * 
+ * Show city and postal_code associated with the property
+ *
+ * @param WP_Post|int|null $post [explicite description]
+ *
+ * @return void
+ */
+function agence_city(?WP_Post $post=null): void
+{
+    if($post === null){
+        $post = get_post();
+    }
+    $cities = get_the_terms($post, 'property_city');
+    if (empty($cities)) {
+        return;
+    }
+    $city = $cities[0];
+    echo $city->name;
+    $postalCode = get_field('postal_code',$city);
+    echo ' (' . $postalCode . ')';
+}
--- wp-content/themes/agencia/inc/images.php
+++ wp-content/themes/agencia/inc/images.php
@@ -3,4 +3,5 @@ add_action('after_setup_theme', function () {
     //set_post_thumbnail_size(250, 250, true);
     remove_image_size('post_thumbnail');
     add_image_size('post_thumbnail', 250, 250, true);
+    add_image_size('property_carousel', 777, 444, true);
 });
